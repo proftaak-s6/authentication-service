91b14c89da24399f8f6b8738ac866871051d5683
Werkman, Niels <Nielswerkman93@gmail.com>
2019-06-18 13:45:53 +0200

    added OpenApi prod settings

--- src/main/java/nl/fontysproject/authentication/web/AuthenticationApi.java
+++ src/main/java/nl/fontysproject/authentication/web/AuthenticationApi.java
@@ -6,21 +6,18 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import io.swagger.v3.oas.annotations.servers.Server;
-import nl.fontysproject.authentication.web.controller.AuthenticationController;
-import nl.fontysproject.authentication.web.controller.TestController;
-import nl.fontysproject.authentication.web.controller.UserController;
-import nl.fontysproject.authentication.web.controller.filter.AuthorizeFilter;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.util.HashSet;
-import java.util.Set;
 
 @ApplicationPath("")
 @OpenAPIDefinition(info = @Info(
         title = "Rekeningrijden | Authentication API",
         version = "0.0.1"),
-        servers = @Server(url = "http://localhost:8080")
+        servers = {
+                @Server(url = "http://localhost:8080"),
+                @Server(url = "http://auth.fontys-project.nl")
+        }
 )
 @SecuritySchemes(
         @SecurityScheme(name = "jwt",
